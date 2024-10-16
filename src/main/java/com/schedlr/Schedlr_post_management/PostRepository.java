c53c764c23bf64bf6f5172da1e542ac0492b2835
Tyler Davis <112653240+TyDScript23@users.noreply.github.com>
2024-10-16 18:03:08 -0400

    Initial Commit

---
+++ src/main/java/com/schedlr/Schedlr_post_management/PostRepository.java
@@ -0,0 +1,5 @@
+package com.schedlr.Schedlr_post_management;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Integer> {
+}
